26c6fc4c62f8bdd92fac387ab1f35174ce558de2
Agustin <mumugomez19@gmail.com>
2025-09-12 21:40:25 -0300

    test atributos no nulo y nombre no nulo y no vacio

--- src/main/java/Modelo/Persona.java
+++ src/main/java/Modelo/Persona.java
@@ -1,4 +1,38 @@
 package Modelo;
 
 public class Persona {
+    private String nombre;
+    private String apellido;
+    private String DNI;
+    private int edad;
+    private double altura;
+    private int peso;
+
+    public static Persona create(String nombre, String apellido, String DNI, int edad, double altura, int peso) {
+
+        return new Persona(nombre, apellido, DNI, edad, altura, peso);
+    }
+
+    public Persona(String nombre, String apellido, String DNI, int edad, double altura, int peso) throws PersonaExceptionNombre {
+
+
+        if(nombre.isBlank() || nombre==null){
+            throw new PersonaExceptionNombre("El nombre no puede ser nulo ni vacio");
+
+        }
+
+        this.nombre = nombre;
+        this.apellido = apellido;
+        this.DNI = DNI;
+        this.edad = edad;
+        this.altura = altura;
+        this.peso = peso;
+
+    }
+
+
+    public String getApellido() {
+        return this.apellido;
+    }
+
 }
--- src/test/java/Modelo/PersonaTest.java
+++ src/test/java/Modelo/PersonaTest.java
@@ -1,17 +1,32 @@
 package Modelo;
 
+
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
 
 public class PersonaTest {
 
     @Test
-    public void IntanciarPersona_AtributosNoNulo{
+    public void IntanciarPersona_AtributosNoNulo(){
 
-    Persona nuevaPersona= Persona.create("Agustin","Gomez","43611353",21,1.70,100);
+        Persona nuevaPersona= Persona.create("Agustin","Gomez","43611353",21,1.70,100);
 
         Assertions.assertNotNull(nuevaPersona);
         Assertions.assertEquals("Gomez",nuevaPersona.getApellido());
     }
+
+    @Test
+    public void create_nombreVacioONulo_execption(){
+
+        Persona nuevaPersona= Persona.create("Agustin","Gomez","43611353",21,1.70,100);
+
+        Assertions.assertThrows(PersonaExceptionNombre.class, ()->Persona.create(" ","Gomez","43611353",21,1.70,90));
+
+
+
+       // Assertions.assertThrows(PersonaExceptionNombre.class,()->Persona.create(null,"Gomez","43611353",24,1.70,90));
+
+    }
+
 
 }
